105e807b6b948636a5b44e3c4b9a240881d006da
Tyler <foxkeh34@gmail.com>
2024-01-28 16:49:02 -0700

    added Operators.cs and Variables.cs

---
+++ Operators.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int multi1 = 2 * 3;
+		int multi2 = 100 * 4;
+		Console.WriteLine("The product of 2 and 3 is " + multi1);
+		Console.WriteLine("This one is " + multi2 + ".");
+		
+	}
+}
