789a8be45f4d5b0d84c50e9b3af94567a78bd7d5
Tyler <foxkeh34@gmail.com>
2024-01-28 15:44:27 -0700

    test subtract

--- sum prac.cs
+++ sum prac.cs
@@ -7,10 +7,12 @@ public class Program
 		int sum1 = 50 + 100;
 		int sum2 = sum1 + 250;
 		int sum3 = sum2 + 300;
+        int sub4 = sum2 - sum1;
 		double myNum = 19.99D;
 		Console.WriteLine(myNum);
 		Console.WriteLine(sum1);
 		Console.WriteLine(sum2);
 		Console.WriteLine(sum3);
+        Console.WriteLine(sub4);
 	}
 }
