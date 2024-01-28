ee886b067f95e133879eaf86f52ff9e5123c441e
Tyler <foxkeh34@gmail.com>
2024-01-28 14:14:03 -0700

    added firepower power  up

--- video follow minigun.cs
+++ video follow minigun.cs
@@ -3,12 +3,14 @@ using System;
 public class Program
 {
 	public Weapon miniGun;
-	
+	public PowerPup weaponpowerup;
 	public void Main()
 	{
 		miniGun = new Weapon();
-		miniGun.ammoCount++;
+		miniGun.ammoCount += 40;
+		miniGun.firepower += powerUp();
 		Console.WriteLine(miniGun.weaponName + " has ammo count " +  miniGun.ammoCount);
+		Console.WriteLine("the fire power of is now " + miniGun.firepower + "has increased")
 
 	}
 }
@@ -17,4 +19,8 @@ public class Weapon {
 	public int ammoCount = 10;
 	public string weaponName = "Mini Gun";
 	public float firepower = 11.5f;
+}
+
+public class powerUp {
+	public float powerLevel + 2.5f;
 }
