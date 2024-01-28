ab7b6c0226b5e870d27ecbf3a112791cee586b02
Tyler <foxkeh34@gmail.com>
2024-01-28 15:40:20 -0700

    added sum prac

---
+++ sum prac.cs
@@ -0,0 +1,16 @@
+using System;
+					
+public class Program
+{
+	public static void Main(string[]args)
+	{
+		int sum1 = 50 + 100;
+		int sum2 = sum1 + 250;
+		int sum3 = sum2 + 300;
+		double myNum = 19.99D;
+		Console.WriteLine(myNum);
+		Console.WriteLine(sum1);
+		Console.WriteLine(sum2);
+		Console.WriteLine(sum3);
+	}
+}
