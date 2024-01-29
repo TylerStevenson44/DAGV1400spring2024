663b9e0b33de79d2c9a290b068c00cc4ac878764
Tyler <foxkeh34@gmail.com>
2024-01-28 20:35:36 -0700

    cat variables added

--- Variables.cs
+++ Variables.cs
@@ -2,8 +2,26 @@ using System;
 
 public class Program
 {
-    public static void Main()
+    public Visitors yardVisitors;
+    public Food catSnack;
+
+    public void Main()
     {
-        int myNum = 50;
+        yardVisitors = new Visitors();
+		catSnack = new Food();
+       
+        yardVisitors.Cats += catSnack.foodCans;
+        Console.WriteLine(yardVisitors.catName + " puts out " + catSnack.Treats + " treats with each can of food.");
+		Console.WriteLine("So there are " + yardVisitors.Cats + " visitors.");
+		
+		
     }
+}
+public class Visitors {
+    public int Cats = 1;
+    public string catName = "Whiskers";
+}
+public class Food {
+    public int Treats = 5;
+    public int foodCans = 1;
 }
