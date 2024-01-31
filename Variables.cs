3fc7f138ebc81aa2417bc9aa19c21f943651fbcb
Tyler <foxkeh34@gmail.com>
2024-01-30 19:14:42 -0700

    void function with 2 param added

--- Variables.cs
+++ Variables.cs
@@ -10,12 +10,22 @@ public class Program
         yardVisitors = new Visitors();
 		catSnack = new Food();
 		
-        catSnack.Treats += 56;
+        catSnack.Treats += 5;
         yardVisitors.Cats = (catSnack.foodCans + catSnack.Treats / 5);
         Console.WriteLine(yardVisitors.catName + " puts out " + catSnack.Treats + " treats outside with " + catSnack.foodCans + " cans of wet food.");
 		Console.WriteLine("Every food can and every 5 treats brings another visit.");
 		Console.WriteLine("So there are " + yardVisitors.Cats + " visitors.");
+		Totalvisits(1, 5);
+		Totalvisits(3, 15);
+		Totalvisits(6, 30);
+		Totalvisits(12, 60);
+		Totalvisits(24, 120);
+	
+		
     }
+	public void Totalvisits (int food, int treat) {
+		var number = food + treat / 5 ;
+		Console.WriteLine("This many cats have visited! " + number);
 }
 public class Visitors {
     public int Cats;
@@ -23,6 +33,6 @@ public class Visitors {
 }
 public class Food {
     public int Treats = 0;
-    public int foodCans = 6;
+    public int foodCans = 1;
 }
-		
+}
