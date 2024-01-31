cc8ea7b7fd436301e2b5ea5ab0f68e87b4ba2c7f
Tyler <foxkeh34@gmail.com>
2024-01-30 20:20:22 -0700

    moved void func to visitor class
    also changed yardVisitors to Yard.

--- Variables.cs
+++ Variables.cs
@@ -2,46 +2,46 @@ using System;
 
 public class Program
 {
-    public Visitors yardVisitors;
+    public Visitors Yard;
     public Food catSnack;
 
     public void Main()
     {
-        yardVisitors = new Visitors();
+        Yard = new Visitors();
 		catSnack = new Food();
 		
         catSnack.Treats += 5;
-        yardVisitors.Cats = (catSnack.foodCans + catSnack.Treats / 5);
-        Console.WriteLine(yardVisitors.catName + " puts out " + catSnack.Treats + " treats outside with " + catSnack.foodCans + " cans of wet food.");
+        Yard.Cats = (catSnack.foodCans + catSnack.Treats / 5);
+        Console.WriteLine(Yard.catName + " puts out " + catSnack.Treats + " treats outside with " + catSnack.foodCans + " cans of wet food.");
 		Console.WriteLine("Every food can and every 5 treats brings another visit.");
-		Console.WriteLine("If enough Cats visit " + yardVisitors.catName + " gets out the toys.");
-		Console.WriteLine("So there are " + yardVisitors.Cats + " visitors.");
-		Totalvisits(1, 5);
-		Toys(1, 5);
-        Totalvisits(4, 12);
-        Toys(4, 12);
-        Totalvisits(8, 38);
-		Toys(8, 38);
-		Totalvisits(23, 27);
-		Toys(23, 27);
-		
+		Console.WriteLine("If enough Cats visit " + Yard.catName + " gets out the toys.");
+		Console.WriteLine("So there are " + Yard.Cats + " visitors.");
+		Yard.Totalvisits(1, 5);
+		Yard.Toys(1, 5);
+        Yard.Totalvisits(4, 12);
+        Yard.Toys(4, 12);
+        Yard.Totalvisits(8, 38);
+		Yard.Toys(8, 38);
+		Yard.Totalvisits(23, 27);
+		Yard.Toys(23, 27);	
     }
-	public void Totalvisits (int food, int treat) {
+}
+public class Visitors {
+    public void Totalvisits (int food, int treat) {
 		var number = food + treat / 5 ;
 		Console.WriteLine("This many cats have visited! " + number);
     }
     public void Toys (int food, int treat) {
 		var number = food + treat / 5 ;
         if (number > 14) {
-            Console.WriteLine("Theres more than 15 cats! the toys have been brought out.");
+            Console.WriteLine("Theres more than 15 cats! The toys have been brought out.");
         } else {
             Console.WriteLine("Not enough cats to bother with the toys.");
-        }
-    }
-}
-public class Visitors {
+		}
+	}
     public int Cats;
     public string catName = "Whiskers";
+    
 }
 public class Food {
     public int Treats = 0;
