6b7b2db35f34eca54858574aa6230a27b8c4df5a
Tyler <foxkeh34@gmail.com>
2024-01-30 20:08:04 -0700

    if else added

--- Variables.cs
+++ Variables.cs
@@ -14,18 +14,30 @@ public class Program
         yardVisitors.Cats = (catSnack.foodCans + catSnack.Treats / 5);
         Console.WriteLine(yardVisitors.catName + " puts out " + catSnack.Treats + " treats outside with " + catSnack.foodCans + " cans of wet food.");
 		Console.WriteLine("Every food can and every 5 treats brings another visit.");
+		Console.WriteLine("If enough Cats visit " + yardVisitors.catName + " gets out the toys.");
 		Console.WriteLine("So there are " + yardVisitors.Cats + " visitors.");
 		Totalvisits(1, 5);
-		Totalvisits(3, 15);
-		Totalvisits(6, 30);
-		Totalvisits(12, 60);
-		Totalvisits(24, 120);
-	
+		Toys(1, 5);
+        Totalvisits(4, 12);
+        Toys(4, 12);
+        Totalvisits(8, 38);
+		Toys(8, 38);
+		Totalvisits(23, 27);
+		Toys(23, 27);
 		
     }
 	public void Totalvisits (int food, int treat) {
 		var number = food + treat / 5 ;
 		Console.WriteLine("This many cats have visited! " + number);
+    }
+    public void Toys (int food, int treat) {
+		var number = food + treat / 5 ;
+        if (number > 14) {
+            Console.WriteLine("Theres more than 15 cats! the toys have been brought out.");
+        } else {
+            Console.WriteLine("Not enough cats to bother with the toys.");
+        }
+    }
 }
 public class Visitors {
     public int Cats;
@@ -34,5 +46,4 @@ public class Visitors {
 public class Food {
     public int Treats = 0;
     public int foodCans = 1;
-}
 }
