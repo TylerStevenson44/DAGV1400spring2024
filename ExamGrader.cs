b24048164c67999a170e27f1d6b03192e238538d
Tyler <foxkeh34@gmail.com>
2024-01-31 23:58:44 -0700

    ExamGrader/TempAdvisor 1

--- ExamGrader.cs
+++ ExamGrader.cs
@@ -1,41 +1,36 @@
-
 using System;
 
 public class Program
 {
-    public Score GetScore;
-    public void Main()
-    {
-        GetScore = new Score();
-
-        GetScore.Grade(100);
-
-    }
-}
-public class Score
-{
-    public void Grade(int exScore) 
-    {
-        var number = exScore;
-        if (number > 89)  
-        {
-            Console.WriteLine("You got an A!");
-        }
-        else if (number > 79)
-        {
-            Console.WriteLine("You got a B!");
-        }
-        else if (number >  69)
-        {
-            Console.WriteLine("You got a C!");
-        }
-        else if (number >  59)
-        {
-            Console.WriteLine("You got a D!");
-        }
-        else
-        {
-            Console.WriteLine("Oopsie whoopsie you failed UwU try harder next time!");
-        }
-    }
-}
+	public void Main()
+	{
+		Console.WriteLine("Please enter your score! 0-100+");
+        //ask the user for their score!
+		string userInput = Console.ReadLine();
+        //this line the Console.ReadLine takes the response from the user and makes
+       //it the value for the string "userInput
+		int number = int.Parse(userInput);
+        //this line "int.Parse" converts the value from string "userInput" into an int "number"
+		if (number > 89)
+		{
+			Console.WriteLine("You got an A!");
+		}
+		else if (number > 79)
+		{
+			Console.WriteLine("You got a B!");
+		}
+		else if (number > 69)
+		{
+			Console.WriteLine("You got a C!");
+		}
+		else if (number > 59)
+		{
+			Console.WriteLine("You got a D!");
+		}
+		else
+		{
+			Console.WriteLine("Oopsie whoopsie you failed UwU try harder next time!");
+		}
+	}    //the if, if else and else statements decide which letter ranking will be awarded.
+}       //the score from the users input gets run as an int through each equation until it meets a true condition.
+       //Console.WriteLine then provides the user with a letter ranking!
--- TempuratureAdviser.cs
+++ TempuratureAdviser.cs
@@ -8,11 +8,21 @@ public class Program
 		Temp = new Cel();
 		
 		Temp.Tempurature(12);
-    }
+    }//^ some left over code. originally ref'd 'public class Cel' for the score.
+    //changed to Console.Readline and an int.Parse() to ref the users input. 
+    //could remove extra and move all lines below 'public void Tempurature()' 
+    //under 'public void Main()'
 }
-public class Cel { 	
-    public void Tempurature(int celsius){
-        var number = celsius;
+public class Cel 
+{ 	
+    public void Tempurature(int celsius)
+    {
+        Console.WriteLine("What is the tempurature(C)?");
+        //asks user question.
+        string response = Console.ReadLine();
+        //gathers response from the user.
+        int number = int.Parse(response);
+        //converts the users input into an integer(int).
 		if (number < 3) 
         {
 			Console.WriteLine("Cold today tho.");
@@ -25,6 +35,6 @@ public class Cel {
         {
             Console.WriteLine("It should be a nice day outside!");		
 		}
-    }
-}
-
+    }   //takes the converted integer provided from the user and runs it sequentially
+}       //through various equations until it meets a condition that is true,
+        //the Console.Writeline associated with each if/else statement will provide the answer.
