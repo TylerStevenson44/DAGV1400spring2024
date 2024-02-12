6c94acbc9fcbd2fc376c2ad83e99b016ae9602c3
Tyler <foxkeh34@gmail.com>
2024-02-11 20:52:38 -0700

    FavFoods1.cs
    added fav food updated number guesser

--- NumberGuesser1.cs
+++ NumberGuesser1.cs
@@ -1,37 +1,37 @@
 using System;
 
-public class Program
+namespace ConsoleApp1
 {
-	public static void Main()
+	public class Program
 	{
-		Console.WriteLine("Guess from 1-10.");
-		Random rnd = new Random();
-		// ^ generate random number.
-		int random = rnd.Next(1, 11);
-		// ^ make the random number between 1 and 10.
-		int input = 0;
-		Console.WriteLine(random);
-		while (random != input)
-		{// ^ outer loop checks if the input is not equal to the random number.
+		public static void Main()
+		{
+			Console.WriteLine("Guess from 1-10.");
+			Random rnd = new Random();
+			// ^ generate random number.
+			int random = rnd.Next(1, 11);
+			// ^ make the random number between 1 and 10.
+			int input = 0;
 			input = Convert.ToInt32(Console.ReadLine());
 			// ^ convert "input" to an integer.
-			while (input < random)
+			while (random != input)
+			{// ^ outer loop checks if the input is not equal to the random number.
+				while (input < random)
+				{
+					Console.WriteLine("Too low!");
+					Console.ReadLine();
+				}// ^ inner loop checks to see if the input is smaller than the random number.
+				while (input > random)
+				{
+					Console.WriteLine("Too high!");
+					Console.ReadLine();
+				}// ^ second inner loop checks to see if the input is larger than the random number.
+			}
+			while (input == random)
 			{
-				Console.WriteLine("Too low!");
+				Console.WriteLine("Correct guess! The number is " + random + ". Nice. ");
 				Console.ReadLine();
-			}// ^ inner loop checks to see if the input is smaller than the random number.
-
-			while (input > random)
-			{
-				Console.WriteLine("Too high!");
-				Console.ReadLine();
-			}// ^ second inner loop checks to see if the input is larger than the random number.
+			}// ^ if the input equals the random number then its a correct guess!
 		}
-
-		while (input == random)
-		{
-			Console.WriteLine("Correct guess! Nice.");
-			Console.ReadLine();
-		}// ^ if the input equals the random number then its a correct guess!
 	}
 }
