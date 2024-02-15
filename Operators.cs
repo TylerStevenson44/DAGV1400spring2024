da3c2dbea3617b9ac67fbccd384446f51db16f3e
Tyler <foxkeh34@gmail.com>
2024-02-14 17:05:53 -0700

    operators
    added notes.

--- Operators.cs
+++ Operators.cs
@@ -5,9 +5,16 @@ public class Program
 	public static void Main()
 	{
 		int multi1 = 2 * 3;
+		// "int" is our integer "multi1" is the nam of it
+		//multiplication of 2 and 3
 		int multi2 = 100 * 4;
+		//multiplication of 100 and 4
+		int add1 = 2 + 6;
 		Console.WriteLine("The product of 2 and 3 is " + multi1);
+		// show the answer to the user
 		Console.WriteLine("This one is " + multi2 + ".");
+		Console.WriteLine(add1)
+		// just the answer
 		
 	}
 }
